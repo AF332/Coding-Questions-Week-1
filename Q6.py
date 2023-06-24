7839a83aee1acd2b6b06b686d55017b6a5c4b9a0
AF332 <134966071+AF332@users.noreply.github.com>
2023-06-24 13:59:56 +0100

    Add files via upload

---
+++ Q6.py
@@ -0,0 +1,13 @@
+# Question 6
+# Create a variable to hold a 4 digit number
+# Use indexing of the string to individually print each number in a different line
+
+four_digit = input("Enter a four digit number: ")
+
+print(four_digit[0])
+print(four_digit[1])
+print(four_digit[2])
+print(four_digit[3])
+
+# very scummy way of doing it 
+# Also floats and integers can't use indexing but lists etc can cos they are subsricptable
