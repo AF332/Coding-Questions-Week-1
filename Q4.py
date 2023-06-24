7824753648268a826ab3ef6a1689560d8e136edd
AF332 <134966071+AF332@users.noreply.github.com>
2023-06-24 12:15:08 +0100

    Add files via upload

--- Q4.py
+++ Q4.py
@@ -1,5 +1,9 @@
 # Question 4
-# 3 variables are created to retain the 
+# 3 variables are created to retain the annual interest rate, number of years, and loan amount.
+# All the retained values must be changed from string into float.
+# Monthly interest rate is calculated by using the annual interest rate and dividing by 100 to convert into decimals and then dividing by 12, the number of months in a year.
+# Monthly payment is calculated using the previously calculated monthly interest rate and multipling it with the loan amount (MIGHT BE WRONG).
+# total payment is then calculated by multipling the loan amount with the number of years and also the annual interest rate in decimal form. 
 
 annual_interest_rate = input("Enter annual interest rate: ")
 no_years = input("Enter number of years: ")
