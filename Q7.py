ecc3d62d022097cf2ff2d3a4b4407fc24015b3c2
AF332 <134966071+AF332@users.noreply.github.com>
2023-06-24 22:37:37 +0100

    Add files via upload

--- Q7.py
+++ Q7.py
@@ -5,15 +5,17 @@
 # Use BMI formula to calculate the BMI.
 # Print out the result.
 
+import math
+
 Weight_lbs = input("Enter weight in lbs: ")
 Height_inches = input("Enter height in inches: ")
 
 Weight_lbs = float(Weight_lbs)
 Height_inches = float(Height_inches)
 
-Weight_kg = Weight_lbs * (1 / 0.45359237)
-Height_meters = Height_inches * (1 / 0.0254)
+Weight_kg = Weight_lbs * 0.45359237
+Height_meters = Height_inches * 0.0254
 
-BMI = Weight_kg / sqrt(Height_meters)
+BMI = Weight_kg / math.sqrt(Height_meters)
 
 print(BMI)
